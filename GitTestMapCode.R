780d6a50e207df23da4f0cd8b12fe2b76402341e
Melissa Ward <maward@ucdavis.edu>
2019-10-16 14:12:00 -0700

    Create GitTestMapCode.R

---
+++ GitTestMapCode.R
@@ -0,0 +1,10 @@
+
+library(ggmap)
+library(maps)
+library(mapdata)
+
+states <- map_data("state")
+CA <- subset(states, region %in% c("california"))
+ggplot(data = CA) + 
+  geom_polygon(aes(x = long, y = lat, group = group), fill = "gray", color = "black") + 
+  coord_fixed(1.3) + theme_bw()
