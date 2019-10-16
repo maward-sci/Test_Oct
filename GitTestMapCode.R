f4a28b1acf3bfd96759401baca7770f8eaefc54d
Hannah Palmer <hmpalmer@ucdavis.edu>
2019-10-16 14:24:17 -0700

    Update GitTestMapCode.R
    added point map

--- GitTestMapCode.R
+++ GitTestMapCode.R
@@ -7,4 +7,9 @@ states <- map_data("state")
 CA <- subset(states, region %in% c("california"))
 ggplot(data = CA) + 
   geom_polygon(aes(x = long, y = lat, group = group), fill = "gray", color = "black") + 
-  coord_fixed(1.3) + theme_bw()
+  coord_fixed(1.3)
+
+ggplot(data = CA) + 
+  geom_point(aes(x = long, y = lat))+
+  theme_classic()
+
